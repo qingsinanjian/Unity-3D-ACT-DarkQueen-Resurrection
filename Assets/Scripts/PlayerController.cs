8a86d25f0c2bd047bc5b75ee831b6281dd4acae4
DESKTOP-6FGN5NP\Egoist <13260963605wjx@gmail.com>
2023-07-05 21:25:37 +0800

    初步完成玩家控制器

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float inputH;
+    private float inputV;
+    private Rigidbody rigid;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+    //private Vector3 inputDir;
+
+    private bool isGround = true;
+    public float jumpForce;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        inputH = Input.GetAxis("Horizontal");
+        inputV = Input.GetAxis("Vertical");
+        if(inputH != 0 && inputV != 0)
+        {
+            float targetRotation = rotateSpeed * inputH;
+            transform.eulerAngles = Vector3.up * Mathf.Lerp(transform.eulerAngles.y, transform.eulerAngles.y + targetRotation, Time.deltaTime);
+        }
+        //inputDir = new Vector3(inputH, 0, inputV);
+
+        if(Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigid.AddForce(Vector3.up * jumpForce);
+            isGround = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //if (inputH != 0 || inputV != 0)
+        //{
+        //    rigid.MovePosition(transform.position + transform.TransformDirection(inputDir) * Time.deltaTime * moveSpeed);
+        //}
+
+        if(inputV != 0)
+        {
+            rigid.MovePosition(transform.position + transform.forward * Time.deltaTime * moveSpeed * inputV);
+        }
+        else
+        {
+            if(inputH != 0)
+            {
+                rigid.MovePosition(transform.position + transform.right * Time.deltaTime * moveSpeed * inputH);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!isGround)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGround = true;
+            }
+        }
+    }
+}
